57a97d9a0aaa79b004037f2ded22ddcecf238cfa
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-03-30 19:06:38 +0530

    scrollbar width

--- frontend/src/pages/TeacherPanel.jsx
+++ frontend/src/pages/TeacherPanel.jsx
@@ -450,7 +450,7 @@ const TeacherPanel = () => {
         </div>
 
         {showAnswerSheets && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4"   style={{ scrollbarWidth: "none"}} >
             <div className="bg-gray-800 rounded-xl p-6 max-w-4xl w-full max-h-[80vh] overflow-y-auto">
               <div className="flex justify-between items-center mb-6">
                 <h2 className="text-2xl font-bold text-white">Submitted Answer Sheets</h2>
@@ -521,7 +521,7 @@ const TeacherPanel = () => {
         )}
 
         {showQuestionSets && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4" style={{ scrollbarWidth: "none"}} >
             <div className="bg-gray-800 rounded-xl p-6 max-w-4xl w-full max-h-[80vh] overflow-y-auto">
               <div className="flex justify-between items-center mb-6">
                 <h2 className="text-2xl font-bold text-white">Question Sets</h2>
