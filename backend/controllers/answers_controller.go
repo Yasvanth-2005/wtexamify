1c6631abde60b314a3e037df7aac358726b2cdfc
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-04-04 10:02:55 +0530

    passcode changed

--- backend/controllers/answers_controller.go
+++ backend/controllers/answers_controller.go
@@ -18,111 +18,6 @@ import (
 
 var answerSheetCollection *mongo.Collection = config.GetCollection(config.Client, "answersheets")
 
-// func CreateAnswerSheet(c *gin.Context) {
-// 	studentName, _ := c.Get("name")
-// 	studentEmail, _ := c.Get("email")
-// 	containerID, _ := c.Get("container_id")
-
-// 	var request struct {
-// 		ExamID string `json:"exam_id"`
-// 	}
-// 	if err := c.ShouldBindJSON(&request); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
-// 		return
-// 	}
-
-// 	examID, err := primitive.ObjectIDFromHex(request.ExamID)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid exam ID"})
-// 		return
-// 	}
-
-// 	// Check if an answer sheet already exists for this student and exam
-// 	var existingAnswerSheet models.AnswerSheet
-// 	err = answerSheetCollection.FindOne(context.TODO(), bson.M{"exam_id": examID, "student_email": studentEmail}).Decode(&existingAnswerSheet)
-// 	if err == nil {
-// 		c.JSON(http.StatusOK, gin.H{"message": "Answer sheet already exists", "answerSheet": existingAnswerSheet})
-// 		return
-// 	}
-
-// 	// Fetch the exam data
-// 	var exam models.Exam
-// 	err = examCollection.FindOne(context.TODO(), bson.M{"_id": examID}).Decode(&exam)
-// 	if err != nil {
-// 		c.JSON(http.StatusNotFound, gin.H{"error": "Exam not found"})
-// 		return
-// 	}
-
-// 	// Validate if there are question sets available
-// 	if len(exam.Sets) == 0 {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "No question sets available for this exam"})
-// 		return
-// 	}
-
-// 	// Select a random question set
-// 	rand.Seed(time.Now().UnixNano())
-// 	selectedSetID := exam.Sets[rand.Intn(len(exam.Sets))]
-
-// 	var questionSet models.QuestionSet
-// 	err = questionSetCollection.FindOne(context.TODO(), bson.M{"_id": selectedSetID}).Decode(&questionSet)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch question set"})
-// 		return
-// 	}
-
-// 	data := make([]map[string]string, len(questionSet.Questions))
-// 	for i, q := range questionSet.Questions {
-// 		data[i] = map[string]string{q: ""}
-// 	}
-
-// 	answerSheetID := primitive.NewObjectID()
-// 	answerSheet := models.AnswerSheet{
-// 		ID:           answerSheetID,
-// 		ExamID:       examID,
-// 		ExamType:     exam.ExamType,
-// 		Duration:     exam.Duration,
-// 		SetNumber:    questionSet.SetNumber,
-// 		StudentName:  studentName.(string),
-// 		StudentEmail: studentEmail.(string),
-// 		Data:         data,
-// 		Copied:       false,
-// 		CopyCount:    0,
-// 		SubmitStatus: false,
-// 		CreatedAt:    primitive.NewDateTimeFromTime(time.Now()),
-// 	}
-
-// 	_, err = answerSheetCollection.InsertOne(context.TODO(), answerSheet)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create answer sheet"})
-// 		return
-// 	}
-
-// 	// Update Student Container with examId and answerSheetId
-// 	studentContainerID, ok := containerID.(primitive.ObjectID)
-// 	if ok {
-// 		update := bson.M{
-// 			"$push": bson.M{
-// 				"question_papers": bson.M{
-// 					"exam_id":         examID,
-// 					"answer_sheet_id": answerSheetID,
-// 					"copied":          false,
-// 				},
-// 			},
-// 		}
-
-// 		_, err = studentContainerCollection.UpdateOne(context.TODO(), bson.M{"_id": studentContainerID}, update)
-// 		if err != nil {
-// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update student container"})
-// 			return
-// 		}
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message":     "Answer sheet created successfully",
-// 		"answerSheet": answerSheet,
-// 	})
-// }
-
 func CreateAnswerSheet(c *gin.Context) {
 	studentName, _ := c.Get("name")
 	studentEmail, _ := c.Get("email")
@@ -453,7 +348,7 @@ func RemoveCopied(c *gin.Context) {
 	}
 
 	// Verify passcode
-	if request.Passcode != "jersey" {
+	if request.Passcode != "wtlabexam" {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid passcode"})
 		return
 	}
