1ddc8e1439a7e28de13929bf5a498b9335f254a5
Maheshkarri4444 <maheshkarri2109@gmail.com>
2025-11-13 23:35:51 +0530

    updated backend urls

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -1,6 +1,6 @@
 const backends = [
-  "https://wtexamifybackend1.onrender.com",
-  "https://wtexamifybackend2.onrender.com",
+  "https://labexamifyrgukt1.onrender.com",
+  "https://labexamify2.onrender.com"
 ];
 
 // const backends = ["http://localhost:8080", "http://localhost:8080"];
